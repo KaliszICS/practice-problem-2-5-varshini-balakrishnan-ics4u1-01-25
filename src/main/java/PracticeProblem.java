310abd4d9ac75d02499b420d57cb7ca2edc14a14
Varshini Balakrishnan <varshini.balakrishnan@student.tdsb.on.ca>
2025-11-03 21:48:58 +0000

    update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,6 +3,17 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+	public static void insertionSort(char[] characters) {
+		for(int i = 1; i < characters.length; i++) {
+			char key = characters[i];
+			int index = i -1;
 
+			while (index >= 0 && Character.toLowerCase(characters[index]) > Character.toLowerCase(key)) {
+				characters[index + 1] = characters[index];
+				index--;
+			}
+			characters[index + 1] = key;
+		}
+	}
 
 }
